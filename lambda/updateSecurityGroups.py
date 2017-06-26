4466a72265e9fce6de2e1160937d8a7db30273a7
Simon So <simon.so@gmail.com>
2017-06-26 09:45:05 -0700

    add policy doc and rename whitelist to ID of security group

--- lambda/updateSecurityGroups.py
+++ lambda/updateSecurityGroups.py
@@ -1,39 +1,50 @@
 import boto3
+import re
 
-sg_id = "sg-7d482705"
 ec2 = boto3.resource("ec2")
-sg = ec2.SecurityGroup(sg_id)
 
 bucket_name = "simonso-access-control"
-file_name = "ip_whitelist"
 s3 = boto3.resource("s3")
+bucket = s3.Bucket(bucket_name)
+
 
 def lambda_handler(event, context):
-    # Read rules list from S3 bucket 
-    obj = s3.Object(bucket_name, file_name)
-    content = obj.get()["Body"].read()
-    desired_rules = content.strip("\n").split("\n")
-    print desired_rules
+    for obj in bucket.objects.all():
+        # TODO: get list of security groups
 
-    # Get existing list of rules from security group
-    actual_rules = []
-    for p in sg.ip_permissions:
-        for cidr_ip in p["IpRanges"]:
-            actual_rules.append(",".join([p["IpProtocol"], str(p["FromPort"]), str(p["ToPort"]), cidr_ip["CidrIp"]]))
-    print actual_rules
+        filename = obj.key
 
-    # Remove rules not in desired_rules
-    for rule in actual_rules:
-        if rule not in desired_rules:
-            print "Removing rule: %s" % rule 
-            r = rule.split(",")
-            sg.revoke_ingress(IpProtocol=r[0],FromPort=int(r[1]),ToPort=int(r[2]),CidrIp=r[3])
+        # Make sure, at the very least, the filenames match the pattern of a security group id
+        # otherwise, move on to the next file
+        if not re.match(r'^sg-[a-z0-9]{8}$', filename):
+            continue
 
-    # Add rules not in actual_rules
-    for rule in desired_rules:
-        if rule not in actual_rules:
-            print "Adding rule: %s" % rule
-            r = rule.split(",")
-            sg.authorize_ingress(IpProtocol=r[0],FromPort=int(r[1]),ToPort=int(r[2]),CidrIp=r[3])
+        # TODO: check if filename matches any of the security groups, if not, move on
+
+        # Read rules list from S3 bucket
+        content = obj.get()["Body"].read()
+        desired_rules = content.strip("\n").split("\n")
+        print desired_rules
+
+        # Get existing list of rules from security group
+        actual_rules = []
+        for p in sg.ip_permissions:
+            for cidr_ip in p["IpRanges"]:
+                actual_rules.append(",".join([p["IpProtocol"], str(p["FromPort"]), str(p["ToPort"]), cidr_ip["CidrIp"]]))
+        print actual_rules
+
+        # Remove rules not in desired_rules
+        for rule in actual_rules:
+            if rule not in desired_rules:
+                print "Removing rule: %s" % rule 
+                r = rule.split(",")
+                sg.revoke_ingress(IpProtocol=r[0],FromPort=int(r[1]),ToPort=int(r[2]),CidrIp=r[3])
+
+        # Add rules not in actual_rules
+        for rule in desired_rules:
+            if rule not in actual_rules:
+                print "Adding rule: %s" % rule
+                r = rule.split(",")
+                sg.authorize_ingress(IpProtocol=r[0],FromPort=int(r[1]),ToPort=int(r[2]),CidrIp=r[3])
 
 
